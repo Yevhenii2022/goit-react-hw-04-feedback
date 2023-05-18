daff5650f7ec0ff3ee8ade61abc6557a670f6064
Yevhenii2022 <3starosta3@gmail.com>
2023-05-18 11:32:06 +0300

    converted into hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,61 +1,76 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Layout } from './Layout';
 import Section from './Section/Section';
 import Statistics from './Statistics/Statistics';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Notification from './Notification/Notification';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+const optionList = {
+  good: 'good',
+  neutral: 'neutral',
+  bad: 'bad',
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const countTotalFeedback = () => {
+    return [good, neutral, bad].reduce((total, val) => total + val, 0);
   };
 
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((total, val) => total + val, 0);
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const total = this.countTotalFeedback();
-    const { good } = this.state;
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
     return Math.round((good * 100) / total) || 0;
   };
 
-  onLeaveFeedback = option => {
-    this.setState(prevState => ({ [option]: prevState[option] + 1 }));
+  const onLeaveFeedback = option => {
+    switch (option) {
+      case optionList.good:
+        setGood(good + 1);
+        break;
+
+      case optionList.neutral:
+        setNeutral(neutral + 1);
+        break;
+
+      case optionList.bad:
+        setBad(bad + 1);
+        break;
+
+      default:
+        throw new Error('Unsupported option type');
+    }
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
 
-    return (
-      <Layout>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.onLeaveFeedback}
-          ></FeedbackOptions>
-        </Section>
+  return (
+    <Layout>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.values(optionList)}
+          onLeaveFeedback={onLeaveFeedback}
+        ></FeedbackOptions>
+      </Section>
 
-        <Section title="Statistics">
-          {total > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            ></Statistics>
-          ) : (
-            <Notification message="There is no feedback"></Notification>
-          )}
-        </Section>
-      </Layout>
-    );
-  }
-}
+      <Section title="Statistics">
+        {total > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          ></Statistics>
+        ) : (
+          <Notification message="There is no feedback"></Notification>
+        )}
+      </Section>
+    </Layout>
+  );
+};
 
 export default App;
